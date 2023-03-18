ca63ad5828e2ae31c99c7e654b1e0b705a5e3a02
George Ikechi Onyekachi <emmag8657@gmail.com.com>
2023-03-18 15:37:45 -0700

    Programming is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints an output using puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building mnltilingual puzzle);
+	return (0);
+}
