55fa2ec0bbc23683c54f91db714d2cde0afd4d0b
George Ikechi Onyekachi <emmag8657@gmail.com.com>
2023-03-24 07:18:49 -0700

    numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,9 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2' && c == '4'))
+		if (!(c == '2' || c == '4'))
 			putchar(c);
 	}
-	putchar(c);
+
+	putchar('\n');
 }
