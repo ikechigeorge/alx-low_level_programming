6fa87193ae51d361766427aeb28ff60dc87a90b3
George Ikechi Onyekachi <emmag8657@gmail.com>
2023-04-04 21:17:37 +0000

    replacing a function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memset - A function that fills memory
+ * @s: A pointer to the memory area
+ * @b; The constant byte to be used
+ * @n: The number of bytes to be filled
+ *
+ * Return: change array with new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int u = 0;
+
+	for (; n > 0; u++)
+	{
+		s[u] = b;
+		n--;
+	}
+	return (s);
+}
