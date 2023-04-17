ccd6ae81d088340c2074e77b1883c28825374e6d
George Ikechi Onyekachi <emmag8657@gmail.com>
2023-04-17 20:20:40 +0000

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,14 @@
 
 #define typedef
 #define STRUCT_DOG
+
+/**
+ * struct dog - entry point
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
