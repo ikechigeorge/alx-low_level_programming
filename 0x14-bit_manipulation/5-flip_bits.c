41a031e29adab34f4148f3beb26bba17bf525c6a
George Ikechi Onyekachi <emmag8657@gmail.com>
2023-05-10 08:36:52 +0000

    file handling in C programming

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int tmp = n ^ m;
+	unsigned p_bits.clong int tmp = n ^ m;
 	unsigned int count = 0;
 
 	while (tmp > 0)
