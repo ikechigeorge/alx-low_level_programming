d035e16ea2359ca8a5e5d1b0db76ed8043c81a27
George Ikechi Onyekachi <emmag8657@gmail.com>
2023-11-10 22:33:40 +0100

    pointers and array

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,9 +14,11 @@ char *string_toupper(char *n)
 	change = 0;
 	while (n[change] != '\0')
 	{
-		if (n[change] >= 'a' && n[change] <= 'z')
+		if (n[change] >= 97 && n[change] <= 122)
+		{
 			n[change] = n[change] - 32;
 			change++;
+		}
 	}
 	return (n);
 }
